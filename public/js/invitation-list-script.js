46b9b1af672e0d46b70e8efeb381d46cde23fe00
bekalearner <bekalearner@gmail.com>
2023-03-03 12:49:19 +0800

    bug fixed

--- public/js/invitation-list-script.js
+++ public/js/invitation-list-script.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const $guestInfo = document.querySelectorAll('.guest')
+const $messageStatusBlock = document.querySelector('.message__status')
 const $messageSendingStatus = document.querySelector('#sending')
 const $messageSuccesStatus = document.querySelector('#success')
 
@@ -9,6 +10,7 @@ $guestInfo.forEach( guest => {
         event.preventDefault
         
         if (event.target.tagName == 'BUTTON'){
+            $messageStatusBlock.classList.remove('hidden')
             $messageSendingStatus.classList.remove('hidden')
             fetch('/api/list', {
                 method: 'POST',
@@ -23,6 +25,7 @@ $guestInfo.forEach( guest => {
                 if( data.status === 200 ) {
                     $messageSuccesStatus.classList.remove('hidden')
                     const timer = setTimeout(() => {
+                        $messageStatusBlock.classList.add('hidden')
                         $messageSuccesStatus.classList.add('hidden')
                     }, 2000)
                 }
